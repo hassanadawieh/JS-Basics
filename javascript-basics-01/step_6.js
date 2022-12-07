6b2d1c512336272b28ed7579c98aa7800ef42646
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-07 16:00:15 +0200

    completed step_6

---
+++ javascript-basics-01/step_6.js
@@ -0,0 +1,5 @@
+document.getElementById("validate").onclick = function () {
+  var number1 = document.getElementById("first_number").value;
+  var number2 = document.getElementById("second_number").value;
+  alert(number1 / number2);
+};
