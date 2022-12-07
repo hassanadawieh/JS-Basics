0128697d673b4741a0cd399599d7086edc34c2d1
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-07 16:12:47 +0200

    completed step_7

--- javascript-basics-01/step_7.js
+++ javascript-basics-01/step_7.js
@@ -1,1 +1,6 @@
 
+document.getElementById("validate").onclick = function () {
+  var shoe_size = document.getElementById("shoe_size").value;
+  var year_birth = document.getElementById("year").value;
+  alert(((shoe_size * 2)+ 5 ) * 50 -year_birth+1766)
+};
