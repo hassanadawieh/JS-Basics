e3fb01ccff34252e7a22c5a7d43efd5b25db78df
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-07 15:50:03 +0200

    completed step_4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,17 @@
+
+// function button(){
+   
+// var name = document.getElementById("name").value;
+// var surname = document.getElementById("surname").value;
+// var city = document.getElementById("city").value;
+
+//  alert(`Name : ${name}\n Surname : ${surname}\n city : ${city}\n welcome`);
+
+// }
+document.getElementById("validate").onclick = function (){
+  var name = document.getElementById("name").value;
+  var surname = document.getElementById("surname").value;
+  var city = document.getElementById("city").value;
+
+  alert(`Name : ${name}\n Surname : ${surname}\n city : ${city}\n welcome`);
+}; 
