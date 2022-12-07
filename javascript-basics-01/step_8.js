49f10ea59f200856f42c9957bfd8fc697410b0ba
Hassan Adawie <adawiehhassan@gmail.com>
2022-12-07 16:48:32 +0200

    completed step_8

---
+++ javascript-basics-01/step_8.js
@@ -0,0 +1,9 @@
+var age = document.getElementById("age").value;
+document.getElementById("validate").onclick = function(){
+    var age = document.getElementById("age").value;
+    if(age>18){
+        alert("you are over 18");
+    }else{
+        alert("you are under 18");
+    }
+}
